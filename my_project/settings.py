2fd047cb35c08c2fde080830777c4ed9add1933c
carlos <carlosmastache301@gmail.com>
2023-02-26 18:32:42 -0600

    uwsgi_params

--- my_project/settings.py
+++ my_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
 import environ
 env = environ.Env()
 environ.Env.read_env()
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 LOGIN_URL = '/blog/login'
 
